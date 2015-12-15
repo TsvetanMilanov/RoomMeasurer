53e55baaba427a4d026171273d346d2abbbb72e9
todorm85 <todorbm85@gmail.com>
2015-12-15 18:36:27 +0200

    Initial commit
    Added logic for focal length calculator and edge distances calculator.

---
+++ RoomMeasurer.Logic/Measurer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace RoomMeasurer.Logic
+{
+    public class Measurer
+    {
+        public List<double> GetEdgeDistances(
+            double[] projectedEdgeHeights,
+            double focalDistance,
+            double projectedReferenceHeight,
+            double actualReferenceHeight
+            )
+        {
+            double realEdgeHeight = GetRealHeight(projectedEdgeHeights[0], projectedReferenceHeight, actualReferenceHeight);
+
+            var edgeDistances = new List<double>();
+            foreach (var projectedHeight in projectedEdgeHeights)
+            {
+                double scale = realEdgeHeight / projectedHeight;
+                var distance = scale * focalDistance;
+                edgeDistances.Add(distance);
+            }
+
+            return edgeDistances;
+        }
+
+        public double GetCameraFocalDistance(double distance, double actualHeight, double projectedHeight)
+        {
+            return distance * projectedHeight / actualHeight;
+        }
+
+        private static double GetRealHeight(double projectedHeight, double projectedReferenceHeight, double actualReferenceHeight)
+        {
+            var scale = actualReferenceHeight / projectedReferenceHeight;
+            var realHeight = projectedHeight * scale;
+            return realHeight;
+        }
+    }
+}

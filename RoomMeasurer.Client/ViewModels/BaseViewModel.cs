13368d098578869f25e127a6e80277537e808f32
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2015-12-16 16:03:54 +0200

    Created the initial structure of the client project.

---
+++ RoomMeasurer.Client/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace RoomMeasurer.Client.ViewModels
+{
+    using System.ComponentModel;
+
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged == null)
+            {
+                return;
+            }
+
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ RoomMeasurer.Client/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RoomMeasurer.Client.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class MainPageViewModel : BaseViewModel
+    {
+    }
+}

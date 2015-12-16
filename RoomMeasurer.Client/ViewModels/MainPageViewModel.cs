f283ef009407394a5ef1ed0904eef4e5ef9a2575
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2015-12-16 19:11:56 +0200

    Added Navigation Service, Delegate Command and added button in the main page to navigate to Go To Measure From Existing Image Page.

--- RoomMeasurer.Client/ViewModels/MainPageViewModel.cs
+++ RoomMeasurer.Client/ViewModels/MainPageViewModel.cs
@@ -1,11 +1,26 @@
 ﻿namespace RoomMeasurer.Client.ViewModels
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
+    using System.Windows.Input;
+    using Utilities;
+
+    using Contracts;
+    using Pages;
 
     public class MainPageViewModel : BaseViewModel
     {
+        public MainPageViewModel()
+        {
+            this.NavigationService = new NavigationService();
+            this.GoToMeasureFromExistingImageCommand = new DelegateCommand(this.HandleGoToMeasureFromExistingImage);
+        }
+
+        public ICommand GoToMeasureFromExistingImageCommand { get; set; }
+
+        public INavigationService NavigationService { get; private set; }
+
+        private void HandleGoToMeasureFromExistingImage()
+        {
+            this.NavigationService.Navigate(typeof(MeasureFromExistingImagePage));
+        }
     }
 }

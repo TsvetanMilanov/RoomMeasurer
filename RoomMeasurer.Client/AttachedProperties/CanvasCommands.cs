61a68f4dcca338c8572c4f1a7cd4a7e995c08866
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2015-12-16 22:37:03 +0200

    Added the layout of Measure From Existing Image Page. Implemented choosing the image and added command for tap event of the canvas.

---
+++ RoomMeasurer.Client/AttachedProperties/CanvasCommands.cs
@@ -0,0 +1,39 @@
+﻿namespace RoomMeasurer.Client.AttachedProperties
+{
+    using System.Windows.Input;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    public class CanvasCommands
+    {
+        public static ICommand GetTap(DependencyObject obj)
+        {
+            return (ICommand)obj.GetValue(TapProperty);
+        }
+
+        public static void SetTap(DependencyObject obj, ICommand value)
+        {
+            obj.SetValue(TapProperty, value);
+        }
+        
+        public static readonly DependencyProperty TapProperty =
+            DependencyProperty.RegisterAttached("Tap", typeof(ICommand), typeof(object), new PropertyMetadata(null, HandleTappedEvent));
+
+        private static void HandleTappedEvent(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var canvas = d as Canvas;
+
+            if (canvas == null)
+            {
+                return;
+            }
+
+            canvas.Tapped += (obj, args) =>
+            {
+                ICommand command = e.NewValue as ICommand;
+
+                command.Execute(args);
+            };
+        }
+    }
+}

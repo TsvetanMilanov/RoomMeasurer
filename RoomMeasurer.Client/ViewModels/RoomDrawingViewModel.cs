733e7080aceedc343420033b530794596fad0f8f
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2015-12-20 10:21:00 +0200

    Added Translate gesture to room polygon.

--- RoomMeasurer.Client/ViewModels/RoomDrawingViewModel.cs
+++ RoomMeasurer.Client/ViewModels/RoomDrawingViewModel.cs
@@ -1,8 +1,15 @@
 ﻿namespace RoomMeasurer.Client.ViewModels
 {
     using System;
+    using System.Windows.Input;
     using Windows.Foundation;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Input;
     using Windows.UI.Xaml.Media;
+    using Windows.UI.Xaml.Shapes;
+    using Windows.UI.Xaml.Controls;
+
+    using Utilities;
 
     public class RoomDrawingViewModel : BaseViewModel
     {
@@ -11,7 +18,13 @@
         public RoomDrawingViewModel()
         {
             this.roomCorners = new PointCollection();
+            this.Translate = new DelegateCommandWithParameter<ManipulationDeltaRoutedEventArgs>(this.ExecuteTranslateCommand);
+            this.DisableInertia = new DelegateCommandWithParameter<ManipulationInertiaStartingRoutedEventArgs>(this.ExecuteDisableInertiaCommand);
         }
+
+        public ICommand Translate { get; set; }
+
+        public ICommand DisableInertia { get; set; }
 
         public PointCollection RoomCorners
         {
@@ -37,15 +50,31 @@
             }
         }
 
+        private void ExecuteDisableInertiaCommand(ManipulationInertiaStartingRoutedEventArgs obj)
+        {
+            obj.TranslationBehavior.DesiredDeceleration = int.MaxValue;
+        }
+
+        private void ExecuteTranslateCommand(ManipulationDeltaRoutedEventArgs obj)
+        {
+            Polygon control = obj.OriginalSource as Polygon;
+
+            double top = Canvas.GetTop(control);
+            double left = Canvas.GetLeft(control);
+
+            Canvas.SetTop(control, top + obj.Delta.Translation.Y);
+            Canvas.SetLeft(control, left + obj.Delta.Translation.X);
+        }
+
         internal void CalculateRoomCorners(RoomGeometryViewModel room)
         {
-            //room = new RoomGeometryViewModel(new System.Collections.Generic.List<double>
-            //{
-            //    50, 50, 50
-            //}, new System.Collections.Generic.List<double>
-            //{
-            //    0, 120, 240
-            //});
+            room = new RoomGeometryViewModel(new System.Collections.Generic.List<double>
+            {
+                50, 50, 50
+            }, new System.Collections.Generic.List<double>
+            {
+                0, 120, 240
+            });
 
             PointCollection roomCorners = new PointCollection();
             for (int i = 0; i < room.Yaws.Count; i++)
